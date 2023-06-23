730194a36e37caa61ae2245b153e75f98dcfce83
Joselyne03 <joselynemalan@gmail.com>
2023-06-22 22:11:33 -0700

    data for the list of product is created

--- student-store-express-api/app.js
+++ student-store-express-api/app.js
@@ -1,1 +1,24 @@
 // YOUR CODE HERE
+//ask if you need to add the 
+//imports here:
+const express = require("express");
+const morgan = require("morgan");
+const productModel = require("./model/Store");
+
+//the middleware here
+const app = express();
+app.use(morgan("dev"));
+app.use(express.json());
+
+//the routes here
+app.get("/", (request,response) => {
+    response.status(200)
+  })
+
+app.get("/store", (request,response) => {
+    const productsList = productModel.getAllProducts();
+    response.json(productsList)
+});
+
+//export app
+module.exports = app;
--- student-store-express-api/server.js
+++ student-store-express-api/server.js
@@ -1,7 +1,10 @@
+//imports are here
 const app = require("./app")
 
+//the express appplication
 const port = process.env.PORT || 3001
 
+//app.use("/store", app);
 app.listen(port, () => {
   console.log(`🚀 Server listening on port ` + port)
-})
+});
